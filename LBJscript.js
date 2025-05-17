5cfc56f8dc68f1f6a1f81c1ef6f338a7e9a3287c
30229680 <dilraj.deol@ucalgary.ca>
2025-05-17 14:58:28 -0600

    Initial commit

---
+++ LBJscript.js
@@ -0,0 +1,10 @@
+
+  // Called automatically by the button’s onclick
+  function playAlbum() {
+    const player = document.getElementById('audio-player');
+
+    // If you only need “play”, one line is enough
+    player.play();
+
+  }
+
